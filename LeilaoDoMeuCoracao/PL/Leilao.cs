444595e557523b5a5573b696674d5d4562f12daa
felipefahrion <felipe.fahrion@gmail.com>
2020-05-02 19:11:45 -0300

    versao para a 1a entrega

--- LeilaoDoMeuCoracao/PL/Leilao.cs
+++ LeilaoDoMeuCoracao/PL/Leilao.cs
@@ -12,7 +12,7 @@ namespace LeilaoDoMeuCoracao.PL
         public DateTime DataInicio { get; set; }
         public DateTime DataMaxLances { get; set; }
         public double Valor { get; set; }
-        public TipoLeilaoEnum TipoLeilaoEnum { get; set; }
+        public LeilaoStrategy TipoLeilaoStrategy { get; set; }
         public StatusLeilaoEnum StatusLeilaoEnum { get; set; }
         public ICollection<Item> Itens { get; set; }
         public ICollection<Lance> Lances { get; set; }
